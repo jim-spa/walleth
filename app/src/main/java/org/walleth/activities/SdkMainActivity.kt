2c527ac9bba2754c8b63600298a7e4ba176d2bb4
Jesus Fuentes <fuentes.jesus1010010@gmail.com>
2018-03-20 08:08:24 -0500

    Lock screen in portrait mode
    Add comments to a confusing (for me) portion of code -Jesus

--- app/src/main/java/org/walleth/activities/SdkMainActivity.kt
+++ app/src/main/java/org/walleth/activities/SdkMainActivity.kt
@@ -3,6 +3,8 @@ package org.walleth.activities
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import kotlinx.android.synthetic.main.activity_main.*
 import org.kethereum.erc681.isEthereumURLString
 import org.ligi.kaxt.startActivityFromClass
@@ -10,11 +12,16 @@ import org.ligi.kaxt.startActivityFromClass
 class SdkMainActivity : MainActivity() {
 
     fun setGameInfo(newGameActivity: Activity, gameIconId : Int, gameDescrStr : String) {
-        game_icon.setBackgroundResource(gameIconId);
-        game_descr.setText(gameDescrStr);
+        game_icon.setBackgroundResource(gameIconId)
+        game_descr.setText(gameDescrStr)
         if (newGameActivity is Activity)
         game_view.setOnClickListener {
             startActivityFromClass(newGameActivity::class)
         }
     }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
+
 }
